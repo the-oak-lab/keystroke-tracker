05d48d6d71a1989e06a8eec52e4cd1bde0cc3b5e
Michael Asher <33189810+michaelasher@users.noreply.github.com>
2026-02-09 10:08:39 -0500

    Updated rlagging_cheaters.R

--- flagging_cheaters.R
+++ flagging_cheaters.R
@@ -8,12 +8,17 @@ library(jsonlite)
 # Put the file path to your Qualtrics data here:
 data_file <- "[YOUR_FILE_PATH_HERE]"
 
+# Put the name of your preferred ID column here (e.g., PROLIFIC_PID)
+# The default, ResponseId, should work for your Qualtrics data but may not be
+# your preferred identifier.
+id_name <- "ResponseId"
+
 # Data import
 data <- read_csv(data_file, skip = 2) 
 names(data) <- read_csv(data_file, n_max = 0) %>% names()
 
 # ID import
-qid_mappings <- read_csv(data_file)[2, ] # In R, if you don't skip, Row 3 is often index 2
+qid_mappings <- read_csv(data_file)[2, ]
 
 ids <- qid_mappings %>% 
   pivot_longer(everything(), names_to = "export_id") %>%
@@ -25,7 +30,7 @@ ids <- qid_mappings %>%
 
 # Flagging cheaters
 keystroke_question_data <- data %>%
-  select(ResponseId, keystroke_log) %>%
+  select(all_of(id_name), keystroke_log) %>%
   filter(keystroke_log != "") %>%
   mutate(log_data = map(keystroke_log, ~fromJSON(.x)$data)) %>%
   unnest(log_data) %>%
@@ -36,9 +41,9 @@ keystroke_question_data <- data %>%
   ) %>% select(-keystroke_log) %>%
   left_join(ids, by = "qid") %>% 
   ungroup() %>%
-  select(ResponseId, qid, export_id, everything())
+  select(all_of(id_name), qid, export_id, everything())
 
-# participants to be manually reviewed
+# Participants to be manually reviewed
 possible_cheaters <- keystroke_question_data %>%
   group_by(ResponseId) %>% 
   mutate(n_with_pastes = sum(pasted > 0),
