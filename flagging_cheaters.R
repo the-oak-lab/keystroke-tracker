6766af32453c189c53eb8e645825eb630950236f
Michael Asher <33189810+michaelasher@users.noreply.github.com>
2026-02-10 12:58:17 -0500

    Update flagging_cheaters.R

--- flagging_cheaters.R
+++ flagging_cheaters.R
@@ -45,11 +45,11 @@ keystroke_question_data <- data %>%
 
 # Pulling participants to be manually reviewed
 possible_cheaters <- keystroke_question_data %>%
-  group_by(ResponseId) %>% 
+  group_by(all_of(id_name)) %>% 
   mutate(n_with_pastes = sum(pasted > 0),
          n_too_short = sum(too_short)) %>%
   filter(n_with_pastes > 0, n_too_short > 0) %>%
-  select(ResponseId, qid, export_id, n_with_pastes, n_too_short, everything())
+  select(all_of(id_name), qid, export_id, n_with_pastes, n_too_short, everything())
 
 
 
