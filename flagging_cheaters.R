53bd759129f97720a1e34a6608cc5b10669488fb
Michael Asher <33189810+michaelasher@users.noreply.github.com>
2026-02-12 11:37:39 -0500

    Update flagging_cheaters.csv
    Added export option

--- flagging_cheaters.R
+++ flagging_cheaters.R
@@ -8,8 +8,8 @@ library(jsonlite)
 # Put the file path to your Qualtrics data here:
 data_file <- "[YOUR_FILE_PATH_HERE]"
 
-# Put the name of your preferred ID column here (e.g., "PROLIFIC_PID")
-# The default, "ResponseId", should work with your Qualtrics data but may not be
+# Put the name of your preferred ID column here (e.g., PROLIFIC_PID)
+# The default, ResponseId, should work for your Qualtrics data but may not be
 # your preferred identifier.
 id_name <- "ResponseId"
 
@@ -35,6 +35,7 @@ keystroke_question_data <- data %>%
   mutate(log_data = map(keystroke_log, ~fromJSON(.x)$data)) %>%
   unnest(log_data) %>%
   mutate(
+    keystroke_order = map_chr(keystroke_order, ~str_c(.x, collapse = "-")),  
     actual_char_count = nchar(text),
     pasted = pastes > 0,
     too_short = actual_char_count > (keypresses)
@@ -43,15 +44,23 @@ keystroke_question_data <- data %>%
   ungroup() %>%
   select(all_of(id_name), qid, export_id, everything())
 
-# Pulling participants to be manually reviewed
+# Participants to be manually reviewed
 possible_cheaters <- keystroke_question_data %>%
-  group_by(all_of(id_name)) %>% 
+  group_by(ResponseId) %>% 
   mutate(n_with_pastes = sum(pasted > 0),
          n_too_short = sum(too_short)) %>%
   filter(n_with_pastes > 0, n_too_short > 0) %>%
-  select(all_of(id_name), qid, export_id, n_with_pastes, n_too_short, everything())
+  select(ResponseId, qid, export_id, n_with_pastes, n_too_short, everything())
 
 
+# You can export the list of possible cheaters to a CSV if desired.
+# By default the code below will save the CSV to your working directory. 
+# You can run the command getwd() to see where this will be on your computer.
+# You can change your working directory with the "Session" menu in RStudio.
+
+export_file <- "possible_cheaters.csv"
+write_csv(possible_cheaters, export_file)
+message("File saved to: ", normalizePath(export_file))
 
 
 
